5bde9b31aaa5025e07a59d0cc0183c0dc65fccab
RiniyaBenny <riniyabenny1999@gmail.com>
2020-11-05 18:38:49 +0530

    commit after adding testing

---
+++ tests/test_session.py
@@ -0,0 +1,19 @@
+from odoo.tests import common
+
+class TestSession(common.TransactionCase):
+    def setUp(self):
+        super(TestSession,self).setUp()
+        self.session = self.env['openacademy.session']
+        self.session1 = self.employee.create({'name': 'Aerospace session', 'start_date': '11/10/2020',
+                                              'duration': '60.00', 'seats': '10', 'instructor_id': 'Alice',
+                                              'course_id': 'Aerospace', 'attendee_ids': 'Tiya'})
+        self.session2 = self.employee.create({'name': 'Aviation session', 'start_date': '12/10/2020',
+                                              'duration': '120.00', 'seats': '10', 'instructor_id': 'Sunny',
+                                              'course_id': 'Aerospace', 'attendee_ids': 'Miya'})
+
+        def test_compute_taken_seats(self):
+            self.assertEqual(self.session.taken_seats, 1)
+            self.assertEqual(self.session.taken_seats, 1)
+            self.session2.write({'attendee_ids':'Liya'})
+            self.assertEqual(self.session2.taken_seats, '2')
+
